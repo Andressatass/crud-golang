76fda2d6758ae36ea2d4e0b5d07922aee3bac71e
Andressatass <andressa.tasso@usp.br>
2024-05-21 21:42:02 -0300

    feat: Add userRepository

--- infra/db/repository/userRepository.go
+++ infra/db/repository/userRepository.go
@@ -1,6 +1,10 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"crud-golang/infra/db/entities"
+
+	"gorm.io/gorm"
+)
 
 type userRepository struct {
 	db *gorm.DB
@@ -10,4 +14,35 @@ func NewUserRepository(db *gorm.DB) userRepository {
 	return userRepository{
 		db: db,
 	}
+}
+
+func (r userRepository) Create(user entities.User) (uint, error) {
+	result := r.db.Create(&user)
+	if result.Error != nil {
+		return 0, result.Error
+	}
+
+	return user.ID, nil
+}
+
+func (r userRepository) Read() {
+
+}
+
+func (r userRepository) Update(user entities.User) error {
+	result := r.db.Save(user)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (r userRepository) Delete(user entities.User) error {
+	result := r.db.Delete(&user)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
 }
