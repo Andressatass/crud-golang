e9816832725b8bc9129723c0ccf944e07ec54575
Andressatass <andressa.tasso@usp.br>
2024-05-22 22:02:26 -0300

    feat: Adding unit test to user entity

--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.22.2
 require (
 	github.com/iamolegga/enviper v1.4.2
 	github.com/spf13/viper v1.18.2
+	github.com/stretchr/testify v1.8.4
 	gorm.io/driver/mysql v1.5.6
 	gorm.io/gorm v1.25.10
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -18,6 +20,7 @@ require (
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
---
+++ server/viewmodel/userTest.go
@@ -0,0 +1,65 @@
+package viewmodel
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserValidate(t *testing.T) {
+	genericErrorFormat := "invalid-parameter-%s"
+
+	for _, tc := range []struct {
+		name           string
+		userModel      UserModel
+		expectedErrors []error
+	}{
+		{
+			name: "ShouldSucessfullyValidateAll",
+			userModel: UserModel{
+				Id:   "123",
+				Name: "Andressa",
+				Age:  25,
+			},
+		},
+		{
+			name: "ShouldReturnErrorWhenFieldAgeIsNegative",
+			userModel: UserModel{
+				Id:   "123",
+				Name: "Andressa",
+				Age:  -1,
+			},
+			expectedErrors: []error{
+				fmt.Errorf(genericErrorFormat, "age"),
+			},
+		},
+		{
+			name: "ShouldReturnErrorWhenFielsNameIsEmpty",
+			userModel: UserModel{
+				Id:   "123",
+				Name: "",
+				Age:  25,
+			},
+			expectedErrors: []error{
+				fmt.Errorf(genericErrorFormat, "name"),
+			},
+		},
+		{
+			name: "ShouldReturnAllErrors",
+			userModel: UserModel{
+				Id:   "123",
+				Name: "",
+				Age:  -1,
+			},
+			expectedErrors: []error{
+				fmt.Errorf(genericErrorFormat, "age"),
+				fmt.Errorf(genericErrorFormat, "name"),
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.expectedErrors, tc.userModel.Validate())
+		})
+	}
+}
